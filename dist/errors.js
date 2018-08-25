f40929e547bb49ed930a32c51ab196133ee41edb
Dmitry Rodin <madiedinro@gmail.com>
2018-08-25 21:31:51 +0300

    removed build

--- dist/errors.js
+++
@@ -1,7 +0,0 @@
-class NotFoundError extends Error {
-    constructor(message) {
-        super(message || 'NotFound');
-        this.name = 'NotFoundError';
-    }
-}
-//# sourceMappingURL=errors.js.map
--- dist/start.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// import 'module-alias/register';
-// import 'tsconfig-paths/register';
-// import 'source-map-support/register';
-const AppServer_1 = require("@app/AppServer");
-AppServer_1.appServer.start();
-exports.default = AppServer_1.appServer;
-//# sourceMappingURL=start.js.map
